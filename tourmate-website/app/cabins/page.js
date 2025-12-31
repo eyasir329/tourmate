d2ffa6942e45e5d60df1b586867a1688d2e44508
Eyasir Ahamed <eyasir329@gmail.com>
2026-01-01 00:24:32 +0600

    fetching data in a page

--- tourmate-website/app/cabins/page.js
+++ tourmate-website/app/cabins/page.js
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <div>Cabins Page</div>;
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
