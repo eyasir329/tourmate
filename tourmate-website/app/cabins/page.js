4bf324d4aa76343cddb4752991222cd5c0a89a40
Eyasir Ahamed <eyasir329@gmail.com>
2026-01-27 13:50:15 +0600

    using the context api for state management

--- tourmate-website/app/cabins/page.js
+++ tourmate-website/app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const revalidate = 3600; // Revalidate data every hour
 // export const revalidate = 15; // has to be real value
@@ -34,6 +35,7 @@ export default function Page({ searchParams }) {
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
